425cdb0e3a400b72c335ae83982ea87385d481aa
Longyang Tang <atang@Longyangs-MacBook-Pro.local>
2019-03-31 18:21:10 +0800

    update method implementation

--- atang_dictionary.py
+++ atang_dictionary.py
@@ -15,7 +15,7 @@ import sqlite3
 # manipulate the dictionary which exist
 class dictionary:
     
-    def __init__(self, word=None):
+    def __init__(self, word=None, Chinese_definition=None):
         self.word = word
     
     def init_dictionary(self):
@@ -23,8 +23,8 @@ class dictionary:
         c = conn.cursor()
 
         # Create table
-        # c.execute('''CREATE TABLE dictionary
-        #             (id, word, definition, Chinese_definition)''')
+        c.execute('''CREATE TABLE dictionary
+                     (id, word, definition, Chinese_definition)''')
 
         with open("words_alpha.txt") as f:
             read_word = f.readlines()
@@ -40,11 +40,11 @@ class dictionary:
         
         
         c.executemany('INSERT INTO dictionary(id, word, definition, Chinese_definition) VALUES (?,?,?,?)', word_list)
-        conn.commit()
+        conn.commit()           # 由于没有提交 commit() 耽误了1个小时
 
         c.close()
 
-
+    # experimental code for manipulate sqlite3
     def create(self):
         # create a dictionary for store initial word
         conn = sqlite3.connect('atang_dictionary.db')
@@ -59,41 +59,58 @@ class dictionary:
         # We can also close the connection if we are done with it.
         # Just be sure any changes have been committed or they will be lost.
         conn.close()
-        
+       
         
 
-    def insert(self):
-        print('This is insert method')
-        
+    def insert(self, insert_definition):
+        conn = sqlite3.connect('atang_dictionary.db')
+        c = conn.cursor()
+        insert_word = (self.word, insert_definition)
+        c.execute('INSERT INTO dictionary (word, definition) VALUES(?, ?)', insert_word)
+        conn.commit()
+        print('INSERT SUCCESSFUL')
+        c.close()
     
     def delete(self):
         print('This is delete method')
         
-    def update(self):
-        print('This is update method')
-        
-    def query(self):
+    def update(self, update_definition):
         conn = sqlite3.connect('atang_dictionary.db')
         c = conn.cursor()
-        query_word = (self.word,)
-        result = c.execute("SELECT Chinese_definition FROM dictionary WHERE word=?", query_word)
-
-        if c.fetchone() is not None:
-            print(c.fetchone()[3])
-
-        print('Sorry there is no {} definition!'.format(self.word))
-        print('*'*99)
-        print('Please add the meaning of this word.')
+        update_word = (update_definition, self.word)
+        print(self.word)
+        print(update_definition)
+        c.execute('UPDATE dictionary SET definition=? WHERE word=?', update_word)
+        conn.commit()
+        print('UPDATE SUCCESSFUL')
+        c.close()
         
+    def query(self, query_word = 'a'):
+        conn = sqlite3.connect('atang_dictionary.db')
+        c = conn.cursor()
+        # result = c.execute("SELECT Chinese_definition FROM dictionary WHERE word=?", query_word)
+        while True:
+            # c.execute('SELECT * from dictionary WHERE word=?', query_word)
+            if c.fetchone() is not None:
+                print(c.fetchone())
+                print(c.fetchone()[2])
+            else:
+                print('Sorry there is no {} definition!'.format(self.word))
+                print('*'*99)
+                print('Please add the meaning of this word.')
+                definition = input()
+                self.update(definition)
+            break
+            
         c.close()
         
         
 if __name__ == "__main__":
     # 开始时实例对象没有传递参数
     new_dictionary = dictionary(sys.argv[1])
-    #new_dictionary.create()
-    #new_dictionary.query()
-    new_dictionary.init_dictionary()
+    
+    new_dictionary.query()
+ 
     
     
     
