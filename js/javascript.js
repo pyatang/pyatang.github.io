ccacf7b6afb6a30670d9fc6dc242af228c68b510
tanglongyang <ly023_8@163.com>
2023-09-25 10:49:45 +0800

    add upload function

--- js/javascript.js
+++ js/javascript.js
@@ -13,6 +13,44 @@ function loadImage(url, callback) {
 }
 
 
+const imageUrls = [
+  'image1.jpg',
+  'image2.jpg',
+  'image3.jpg',
+  // Add more image URLs as needed
+];
+
+let currentImageIndex = 0;
+const galleryImage = document.getElementById('gallery-image');
+const prevButton = document.getElementById('prev-button');
+const nextButton = document.getElementById('next-button');
+
+function displayImage(index) {
+  galleryImage.src = imageUrls[index];
+}
+
+prevButton.addEventListener('click', () => {
+  if (currentImageIndex > 0) {
+    currentImageIndex--;
+    displayImage(currentImageIndex);
+  }
+});
+
+nextButton.addEventListener('click', () => {
+  if (currentImageIndex < imageUrls.length - 1) {
+    currentImageIndex++;
+    displayImage(currentImageIndex);
+  }
+});
+
+// Initial display
+displayImage(currentImageIndex);
+
+
+
+
+
+
 // Get references to the file input and the button
 const fileInput = document.getElementById('fileInput');
 const uploadButton = document.getElementById('uploadButton');
@@ -35,3 +73,73 @@ fileInput.addEventListener('change', function() {
     // You can also perform any additional processing you need on the selected file.
   }
 });
+
+
+<script>
+  const imageUrls = [
+"../../static/pic/running/20220209185500.jpg",
+                                             ,
+"../../static/pic/running/20220211164114.jpg",
+                                             ,
+"../../static/pic/running/20220217190000.jpg",
+                                             ,
+"../../static/pic/running/20220306151100.jpg",
+                                             ,
+"../../static/pic/running/20220603151100.jpg",
+                                             ,
+"../../static/pic/running/20220605151100.jpg",
+                                             ,
+"../../static/pic/running/20230916101339.jpg",
+                                             ,
+"../../static/pic/running/20230923094034.jpg",
+
+    // Add more image URLs as needed
+  ];
+
+  let currentImageIndex = 0;
+  const galleryImage = document.getElementById('gallery-image');
+  const prevButton = document.getElementById('prev-button');
+  const nextButton = document.getElementById('next-button');
+  const touchArea = document.getElementById('touch-area');
+
+  function displayImage(index) {
+    galleryImage.src = imageUrls[index];
+  }
+
+  prevButton.addEventListener('click', () => {
+    if (currentImageIndex > 0) {
+      currentImageIndex--;
+      displayImage(currentImageIndex);
+    }
+  });
+
+  nextButton.addEventListener('click', () => {
+    if (currentImageIndex < imageUrls.length - 1) {
+      currentImageIndex++;
+      displayImage(currentImageIndex);
+    }
+  });
+
+  // Initialize Hammer.js on the touch area
+  const hammer = new Hammer(touchArea);
+
+  // Add swipe event listeners for left and right swipes
+  hammer.on('swipeleft', () => {
+    if (currentImageIndex < imageUrls.length - 1) {
+      currentImageIndex++;
+      displayImage(currentImageIndex);
+    }
+  });
+
+  hammer.on('swiperight', () => {
+    if (currentImageIndex > 0) {
+      currentImageIndex--;
+      displayImage(currentImageIndex);
+    }
+  });
+
+  // Initial display
+  displayImage(currentImageIndex);
+</script>
+
+
